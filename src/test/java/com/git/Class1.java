13e217df5f6966c0c47e8649e80c045e40422489
UmaTadi <uma.tadi1997@gmail.com>
2024-10-03 11:37:51 +0530

    first commit

---
+++ src/test/java/com/git/Class1.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+System.out.println("hi hello i am uma tadi");
+System.out.println("i am from nagaram");
+	}
+
+}
